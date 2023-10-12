b648b3e7f7d7817f0993ec30e91fcf2e1922243e
DeproOs <klaudio_4ever@hotmail.com>
2023-10-12 12:18:29 -0300

    clase 601 terminada

--- spring-boot-web/src/main/java/com/bolsadeideas/springboot/app/springbootweb/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/app/springbootweb/controllers/IndexController.java
@@ -4,8 +4,10 @@ package com.bolsadeideas.springboot.app.springbootweb.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
     @GetMapping({"/index", "/", "/home" })
