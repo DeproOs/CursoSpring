9c95d4dd6c0a627ce24e4906e5679e6bdba525f0
DeproOs <klaudio_4ever@hotmail.com>
2023-10-12 15:27:20 -0300

    clase 611 terminada

--- spring-boot-web/src/main/java/com/bolsadeideas/springboot/app/springbootweb/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/app/springbootweb/controllers/IndexController.java
@@ -1,10 +1,17 @@
 package com.bolsadeideas.springboot.app.springbootweb.controllers;
 
 
+import com.bolsadeideas.springboot.app.springbootweb.models.Usuario;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/app")
@@ -16,4 +23,36 @@ public class IndexController {
         return "index";
     }
 
+    @RequestMapping(value = "/perfil", method = RequestMethod.GET)
+    public String perfil(Model model){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Claudio");
+        usuario.setApellido("Alvarez");
+        usuario.setEmail("claudio@gmail.com");
+
+        model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+
+        model.addAttribute("usuario", usuario);
+        return "perfil";
+    }
+
+    @RequestMapping(value = "/listar", method = RequestMethod.GET)
+    public String listar(Model model){
+
+        model.addAttribute("titulo", "Listado de usuarios: ");
+
+        return "listar";
+    }
+
+    @ModelAttribute("usuarios")
+    public List<Usuario> poblarUsuarios(){
+        List<Usuario> usuarios= new ArrayList<>();
+
+        usuarios.add(new Usuario("Claudio", "Alvarez", "claudio@gmail.com"));
+        usuarios.add(new Usuario("Jose", "Vidal", "jose@gmail.com"));
+        usuarios.add(new Usuario("Matias", "Lopez", "mati@gmail.com"));
+        usuarios.add(new Usuario("Tornado", "Roe", "tornado@gmail.com"));
+
+        return usuarios;
+    }
 }
